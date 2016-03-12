b88c79915e8fdb493d650ad39508856c19860c0f
mickedav <mikda221@student.liu.se>
2016-03-12 10:19:25 +0100

    Fixed indetion.

--- core.js
+++ core.js
@@ -8,17 +8,15 @@ var bodyParser = require("body-parser");
 var config = require("./config");
 
 var App = {
-	Express: {},
-	Server: {},
-	init: function() {
+		Express: {},
+		Server: {},
+		init: function() {
 		App.Express = express();
-
 		App.Express.use(bodyParser());
-
 		require("./routes")();
 
 		App.Server = App.Express.listen(process.env.PORT || config.port, function() {
-		    console.log("Listening on port %d", App.Server.address().port);
+			console.log("Listening on port %d", App.Server.address().port);
 		});
 	}
 };
